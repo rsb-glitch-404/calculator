ed8886c1b5af68337c31fc67448045a45930467a
raghav <[bhupatiraju.raghav@gmail.com]>
2024-09-06 13:29:50 +0800

    changed the colour of the mouseover and down actionevents

--- calc-script.js
+++ calc-script.js
@@ -1,12 +1,23 @@
 const buttons = document.querySelectorAll(".button")
+const settingsButtons = document.querySelectorAll(".settingsButton")
 
 
 buttons.forEach((button) => {
     button.addEventListener("mouseover", () => {
-        button.style.backgroundColor = "#515151"
+        button.style.backgroundColor = "#616161"
     });
 
     button.addEventListener("mouseout", () => {
-        button.style.backgroundColor = ''
+        button.style.backgroundColor = ""
+    });
+});
+
+settingsButtons.forEach((settingsButton) => {
+    settingsButton.addEventListener("mouseover", () => {
+        settingsButton.style.backgroundColor = "#EB8A00"
+    });
+
+    settingsButton.addEventListener("mouseout", () => {
+        settingsButton.style.backgroundColor = ""
     });
 });
